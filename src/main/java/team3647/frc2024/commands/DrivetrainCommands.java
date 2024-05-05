3548794f5c920c3cace9dd598f8bff1dabd7fdfe
Anirudh S <asaligrama03@gmail.com>
2024-05-05 10:39:39 -0700

    adding thing for autodrive

--- src/main/java/team3647/frc2024/commands/DrivetrainCommands.java
+++ src/main/java/team3647/frc2024/commands/DrivetrainCommands.java
@@ -60,16 +60,16 @@ public class DrivetrainCommands {
                             -turnSpeedFunction.getAsDouble() * maxRotationRadpS * triggerSlow;
 
                     if (mode == DriveMode.SHOOT_AT_AMP && enabeld) {
-                        motionXComponent = autoDriveTwist2d.dx;
-                        motionTurnComponent = autoDriveTwist2d.dtheta;
+                        motionXComponent = autoDriveTwist2d.dx + motionXComponent *0.1;
+                        motionTurnComponent = autoDriveTwist2d.dtheta + motionTurnComponent * 0.1;
 
                         var translation = new Translation2d(motionXComponent, motionYComponent);
 
                         var rotation = motionTurnComponent;
                         swerve.driveFieldOriented(translation.getX(), translation.getY(), rotation);
                     } else if (mode == DriveMode.INTAKE_IN_AUTO && enabeld) {
-                        motionXComponent = autoDriveTwist2d.dx;
-                        motionYComponent = autoDriveTwist2d.dy;
+                        motionXComponent = autoDriveTwist2d.dx  + 0.1 * motionXComponent;
+                        motionYComponent = autoDriveTwist2d.dy + 0.1 * motionYComponent;
 
                         var translation = new Translation2d(motionXComponent, motionYComponent);
 
@@ -77,7 +77,7 @@ public class DrivetrainCommands {
                         swerve.drive(translation.getX(), translation.getY(), rotation);
                     } else {
                         if (mode != DriveMode.NONE && enabeld) {
-                            motionTurnComponent = autoDriveTwist2d.dtheta;
+                            motionTurnComponent = autoDriveTwist2d.dtheta + motionTurnComponent * 0.1;
                         }
                         SmartDashboard.putNumber("theta", autoDriveTwist2d.dtheta);
 
